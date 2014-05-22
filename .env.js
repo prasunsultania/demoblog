f1901eef66f71ed300e9f789f005cd35fd2e7c81
Prasun <prasun.sultania@gmail.com>
2014-05-23 01:37:52 +0530

    start node.js in before

--- .env.js
+++ .env.js
@@ -1,6 +1,6 @@
 
 module. exports = {
-  "NODE_ENV": "development",
+  "NODE_ENV": process.env.NODE_ENV || "development",
   "NODEJS_IP": process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
   "NODEJS_PORT": process.env.OPENSHIFT_NODEJS_PORT || "8080"  
 };
