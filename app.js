20d6318a0ea4e3e00e58761f4a7b286f7764d638
Prasun <prasun.sultania@gmail.com>
2014-05-30 01:24:01 +0530

    cctrl

--- app.js
+++ app.js
@@ -70,7 +70,8 @@ var BlogApp = function() {
     	    
     	    app
     	    .use(connect.vhost('login.localhost.in', loginApp))
-    	    .use(connect.vhost('vast-chamber-5927.herokuapp.com', loginApp))    	    
+    	    .use(connect.vhost('vast-chamber-5927.herokuapp.com', loginApp))
+    	    .use(connect.vhost('awesomeblog.cloudcontrolled.com', loginApp))    	    
     	    .use(connect.vhost('blog-webinvader.rhcloud.com', loginApp))
     	    .use(connect.vhost('prasun.io', loginApp))
     	    .use(connect.vhost('www.prasun.io', loginApp))
